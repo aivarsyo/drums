0ad8a1fd3a1307d6ecec5fb3db55e483bc6e575e
liatalony <liatalony311@gmail.com>
2019-03-28 14:01:34 +0100

    text effects

--- drums.js
+++ drums.js
@@ -7,6 +7,16 @@ const ride = document.querySelector("#ride");
 const snare = document.querySelector("#snare");
 const tink = document.querySelector("#tink");
 const tom = document.querySelector("#tom");
+const BoomText = document.querySelector("#bt1");
+const ClapText = document.querySelector("#bt2");
+const HihatText = document.querySelector("#bt3");
+const KickText = document.querySelector("#bt4");
+const OpenhatText = document.querySelector("#bt5");
+const RideText = document.querySelector("#bt6");
+const SnareText = document.querySelector("#bt7");
+const TinkText = document.querySelector("#bt8");
+const TomText = document.querySelector("#bt9");
+
 
 document.addEventListener("keydown", keys);
 
@@ -14,30 +24,39 @@ function keys(e) {
     console.log("the pressed key is " + e.key + "(no. " + e.which + ")");
     if (e.which == 87) {
         boom.currentTime = 0;
+        BoomText.classList.toggle('enlarge');
         boom.play();
     } else if (e.which == 69) {
         clap.currentTime = 0;
+        ClapText.classList.toggle('enlarge');
         clap.play();
     } else if (e.which == 82) {
         hihat.currentTime = 0;
+        HihatText.classList.toggle('enlarge');
         hihat.play();
     } else if (e.which == 83) {
         kick.currentTime = 0;
+        KickText.classList.toggle('enlarge');
         kick.play();
     } else if (e.which == 68) {
         openhat.currentTime = 0;
+        OpenhatText.classList.toggle('enlarge');
         openhat.play();
     } else if (e.which == 70) {
         ride.currentTime = 0;
+        RideText.classList.toggle('enlarge');
         ride.play();
     } else if (e.which == 88) {
         snare.currentTime = 0;
+        SnareText.classList.toggle('enlarge');
         snare.play();
     } else if (e.which == 67) {
         tink.currentTime = 0;
+        TinkText.classList.toggle('enlarge');
         tink.play();
     } else if (e.which == 86) {
         tom.currentTime = 0;
+        TomText.classList.toggle('enlarge');
         tom.play();
     }
 }
